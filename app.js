7dfda234911972c9f44b8ae86951f79d109b4ed3
smarch323 <smarch323@gmail.com>
2023-05-16 21:55:51 -0400

    update cb domain2

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   // callbackURL: "http://localhost:3000/auth/google/listlog",
-  callbackURL: "https://cute-rose-gharial-slip.cyclic.app/"
+  callbackURL: "https://cute-rose-gharial-slip.cyclic.app/listlog"
 },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile);

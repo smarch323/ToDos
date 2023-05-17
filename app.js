1118cb622a792705bb69d172bf606ba621dfd55f
smarch323 <smarch323@gmail.com>
2023-05-16 21:49:12 -0400

    test run 1

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const _ = require("lodash");
 
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+
 const app = express();
 const PORT = process.env.PORT || 3000; // online cyclic instructions
-
-
 
 
 app.set('view engine', 'ejs');
@@ -17,6 +21,19 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 
 mongoose.set('strictQuery', false);
 // mongoose.connect(process.env.MONGO_URI);
@@ -33,7 +50,84 @@ const connectDB = async () => {
   }
 };
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  name : String,
+  googleId: String,
+  googleName: String,
+});
 
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, {
+      id: user.id,
+      username: user.username,
+      name: user.name
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+//Google
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/listlog",
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOne({
+    googleId: profile.id
+  }).then((foundUser) => {
+    if (foundUser) {
+      return foundUser;
+    } else {
+      const newUser = new User({
+        googleId: profile.id,
+        googleName: profile.displayName,
+      });
+      return newUser.save();
+    }
+  }).then((user) => {
+    return cb(null, user);
+  }).catch((err) => {
+    return cb(err);
+  });
+}
+));
+
+app.get("/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"]
+  }));
+
+app.get("/auth/google/listlog",
+  passport.authenticate("google", {
+    failureRedirect: "/"
+  }),
+  function(req, res) {
+let userPath = req.user.googleName;
+    res.redirect("/" + userPath);
+  });
+
+
+  app.get("/logout", function(req, res) {
+    req.logOut((err) => {});
+    res.redirect("/");
+  })
+
+  
 //should have named itemsSchema
 const itemSchema = new mongoose.Schema({
   name: String
@@ -62,32 +156,38 @@ const listSchema = new mongoose.Schema({
 
 const List = mongoose.model("List", listSchema);
 
+
 app.get("/", function(req, res) {
-
-  //find all returns an array (foundItems - defined here)
-  Item.find({}, function(err, foundItems) {
-
-    if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Succesfully added to itemsDB");
-        }
-      });
-      res.redirect("/");
-    } else {
-      res.render("list", {
-        listTitle: "Today",
-        newListItems: foundItems
-      });
-    }
-
-  });
+  res.render("home");
 });
 
+//OG Home List Page
+// app.get("/", function(req, res) {
+
+//   //find all returns an array (foundItems - defined here)
+//   Item.find({}, function(err, foundItems) {
+
+//     if (foundItems.length === 0) {
+//       Item.insertMany(defaultItems, function(err) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log("Succesfully added to itemsDB");
+//         }
+//       });
+//       res.redirect("/");
+//     } else {
+//       res.render("list", {
+//         listTitle: "Today",
+//         newListItems: foundItems
+//       });
+//     }
+
+//   });
+// });
+
 app.get("/:customListName", function(req, res) {
-  const customListName = _.capitalize(req.params.customListName);
+  const customListName = _.startCase(req.params.customListName);
 
   //returns object with findOne to search within listDB for entered url param
   List.findOne({
@@ -180,6 +280,6 @@ app.post("/delete", function(req, res) {
 //Recommended cyclic connection
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log('Listening on port $(PORT)')
+    console.log('Listening on port '  + PORT);
   })
 });
